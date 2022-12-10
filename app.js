72eb71717f3d67aee82d906c54ac2e46dd4e0ff2
Gerard Solanes Hernandez <gerardsolher@gmail.com>
2022-12-10 14:39:47 +0100

    Implement app logic for pages. Link styles

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    res.status(200).sendFile(__dirname + '/views/home.html');
+})
+
+app.get('/about', (req,res) => {
+    res.status(200).sendFile(__dirname + '/views/about.html');
+})
+
+app.get('/works', (req,res) => {
+    res.status(200).sendFile(__dirname + '/views/works.html')
+})
+
+app.listen(3000);
