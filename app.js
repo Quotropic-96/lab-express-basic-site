6f5bc42093c90300c727334cccec196bfe29a033
Gerard Solanes Hernandez <gerardsolher@gmail.com>
2022-12-14 15:53:20 +0100

    Add layout file

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ app.set('views', __dirname + '/views');
 app.use(express.static('public'));
 
 app.get('/', (req,res) => {
-    res.status(200).sendFile(__dirname + '/views/home.html');
+    res.status(200).render('home');
 })
 
 app.get('/about', (req,res) => {
-    res.status(200).sendFile(__dirname + '/views/about.html');
+    res.status(200).render('about');
 })
 
 app.get('/works', (req,res) => {
-    res.status(200).sendFile(__dirname + '/views/works.html')
+    res.status(200).render('works');
 })
 
 app.listen(3000);
